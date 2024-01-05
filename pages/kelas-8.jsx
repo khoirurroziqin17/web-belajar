e96803fcab5261d476ccf935a55b954625674805
khoirurroziqin17 <khoirurroziqin951@gmail.com>
2024-01-05 09:43:48 +0700

    add: link tugas kelas 8

--- pages/kelas-8.jsx
+++ pages/kelas-8.jsx
@@ -7,6 +7,18 @@ function kelas8() {
       <main className="pt-20 px-6 md:px-8">
         <h2 className="font-semibold text-xl">Daftar Soal</h2>
         <div className="mt-6">
+          <a
+            href="https://scmatsamuga.notion.site/scmatsamuga/Soal-Kelas-8-90f2dccc7b6c4ecc96dbe5bfc803ad43"
+            className="block bg-slate-100 border-2 border-gray-900 p-4 rounded dark:bg-slate-700 dark:border-gray-400"
+            target="_blank"
+            rel="follow"
+          >
+            <h3 className="text-lg font-medium">Tugas Harian</h3>
+            <p>
+              Selesaikan semua tugas harian yang diberikan guru untuk bisa
+              mengikuti ujian akhir semester.
+            </p>
+          </a>
           {/* <a
             href="https://forms.gle/REkt4eDPajSZLdVR9"
             className="block bg-slate-100 border-2 border-gray-900 p-4 rounded"
