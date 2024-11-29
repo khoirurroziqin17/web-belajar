ff379f0b9f1335d63079051455dccf7a0f8701e4
khoirurroziqin17 <khoirurroziqin951@gmail.com>
2024-11-29 10:06:22 +0700

    add: menambahkan link 29/11

--- src/app/pas-2024/[kelas]/[ujian_id]/page.tsx
+++ src/app/pas-2024/[kelas]/[ujian_id]/page.tsx
@@ -1,15 +1,25 @@
 import { Navigation } from "@/components/HeroSection";
+import Image from "next/image";
 
 export default function Page({ params }: { params: { ujian_id: string } }) {
   return (
     <>
       <Navigation />
-      <iframe
-        src={`https://docs.google.com/forms/d/e/${params.ujian_id}/viewform?embedded=true`}
-        className="mx-auto w-full h-[calc(100vh-4rem)] mt-16"
-      >
-        Memuat…
-      </iframe>
+      <div className="w-full mt-16 mx-auto">
+        <Image
+          src={"/header-pas-2024.png"}
+          width={720}
+          height={720}
+          alt="Header PAS 2024"
+          className="mx-auto"
+        />
+        <iframe
+          src={`https://docs.google.com/forms/d/e/${params.ujian_id}/viewform?embedded=true`}
+          className="w-full m-auto h-screen pt-4"
+        >
+          Memuat…
+        </iframe>
+      </div>
     </>
   );
 }
--- src/app/pas-2024/[kelas]/page.tsx
+++ src/app/pas-2024/[kelas]/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import React from "react";
 import { Navigation } from "@/components/HeroSection";
 import Link from "next/link";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const Daftarpelajaran = [
   {
@@ -7,58 +22,83 @@ const Daftarpelajaran = [
     pelajaran: [
       [
         "Bahasa Indonesia",
-        ["1FAIpQLSdFys83_FPsvUegIFXiMvTlwi8Z8KByu2_FuHb9LEvQRtUYiw", "/", "/"],
+        ["1FAIpQLScep5FrGgOSu7p0-702si9OXlQ4GJ62M_njEYrA_8EVeiHujA", "#", "#"],
+        "07.30 - 09.30 WIB",
       ],
-      ["Fikih", ["/", "/", "/"]],
+      ["Fikih", ["#", "#", "#"], "10.00 - 11.30 WIB"],
     ],
   },
   {
     hari: "Selasa, 3 Desember 2024",
     pelajaran: [
-      ["Bahasa Inggris", ["/", "/", "/"]],
-      ["Akidah Akhlak", ["/", "/", "/"]],
+      [
+        "Bahasa Inggris",
+        ["#", "#", "1FAIpQLSf2Y369aTnxIPYvJdK2_YKGXefOKbH3Nnlc_AdaEkqpIiF7XA"],
+        "07.30 - 09.30 WIB",
+      ],
+      ["Akidah Akhlak", ["#", "#", "#"], "10.00 - 11.30 WIB"],
     ],
   },
   {
     hari: "Rabu, 4 Desember 2024",
     pelajaran: [
-      ["Matematika", ["/", "/", "/"]],
-      ["Alquran Hadits", ["/", "/", "/"]],
+      [
+        "Matematika",
+        ["#", "1FAIpQLSfos2F2Js1bG1OsWg8SBsycqU1FHrFBu3h8MxqrWVanhEicCw", "#"],
+        "07.30 - 09.30 WIB",
+      ],
+      ["Alquran Hadits", ["#", "#", "#"], "10.00 - 11.30 WIB"],
     ],
   },
   {
     hari: "Kamis, 5 Desember 2024",
     pelajaran: [
-      ["Ilmu Pengetauan Alam", ["/", "/", "/"]],
-      ["Sejarah Kebudayaan Islam", ["/", "/", "/"]],
+      ["Ilmu Pengetauan Alam", ["#", "#", "#"], "07.30 - 09.30 WIB"],
+      [
+        "Sejarah Kebudayaan Islam",
+        [
+          "1FAIpQLSeJkcllglwozaTwNg855aZjp91boGJ0F6tnFSWWvTf07_lF2Q",
+          "1FAIpQLSelcmBKeCSmre3NTAc1U6XZXXd6EeZ7u6biA15evOg3a21zeA",
+          "1FAIpQLSdw8curUdg3MSLC8RlcJdlAPSQBmTmFcQHo5qB-S7ZwnJXlvQ",
+        ],
+        "10.00 - 11.30 WIB",
+      ],
     ],
   },
   {
     hari: "Jum'at, 6 Desember 2024",
     pelajaran: [
-      ["Bahasa Jawa", ["/", "/", "/"]],
-      ["Ke-NU-An", ["/", "/", "/"]],
+      ["Bahasa Jawa", ["#", "#", "#"], "07.30 - 09.00 WIB"],
+      ["Ke-NU-An", ["#", "#", "#"], "09.15 - 10.45 WIB"],
     ],
   },
   {
     hari: "Senin, 9 Desember 2024",
     pelajaran: [
-      ["Bahasa Arab", ["/", "/", "/"]],
-      ["Seni Budaya", ["/", "/", "/"]],
+      ["Bahasa Arab", ["#", "#", "#"], "07.30 - 09.30 WIB"],
+      ["Seni Budaya", ["#", "#", "#"], "10.00 - 11.30 WIB"],
     ],
   },
   {
     hari: "Selasa, 10 Desember 2024",
     pelajaran: [
-      ["Ilmu Pengetauan Sosial", ["/", "/", "/"]],
-      ["Prakarya", ["/", "/", "/"]],
+      ["Ilmu Pengetauan Sosial", ["#", "#", "#"], "07.30 - 09.30 WIB"],
+      ["Prakarya", ["#", "#", "#"], "10.00 - 11.30 WIB"],
     ],
   },
   {
     hari: "Rabu, 11 Desember 2024",
     pelajaran: [
-      ["Pendidikan Pancasila dan Kewarganegaraan", ["/", "/", "/"]],
-      ["Pendidikan Jasmani, Olahraga, dan Kesehatan", ["/", "/", "/"]],
+      [
+        "Pendidikan Pancasila dan Kewarganegaraan",
+        ["#", "#", "#"],
+        "07.30 - 09.00 WIB",
+      ],
+      [
+        "Pendidikan Jasmani, Olahraga, dan Kesehatan",
+        ["#", "#", "#"],
+        "09.30 - 11.00 WIB",
+      ],
     ],
   },
 ];
@@ -111,21 +151,76 @@ function Card({
 
           {params.pelajaran.map((p) => {
             return (
-              <Link
+              <ModalConfirm
                 key={String(p[0])}
-                href={`${kelas}/${
+                link={`${
                   kelas == "7" ? p[1][0] : kelas == "8" ? p[1][1] : p[1][2]
                 }`}
-                className={
-                  "block rounded-md px-4 py-2 font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                }
+                kelas={kelas}
+                mapel={p[0]}
+                waktu={p[2]}
               >
-                {p[0]}
-              </Link>
+                <AlertDialogTrigger className="block mx-auto px-4 py-2 font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                  {p[0]}
+                </AlertDialogTrigger>
+              </ModalConfirm>
             );
           })}
         </div>
       </div>
     </div>
+  );
+}
+
+function ModalConfirm({ link, kelas, mapel, waktu, children }) {
+  const [disabled, isDisabled] = React.useState<boolean>(true);
+  return (
+    <AlertDialog>
+      {children}
+      <AlertDialogContent className="bg-white dark:bg-neutral-950">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Informasi PAS</AlertDialogTitle>
+          <AlertDialogDescription className="font-medium">
+            <p>Mata Pelajaran : {mapel}</p>
+            <p>
+              Kelas :{" "}
+              {kelas == "7"
+                ? "Tujuh (7)"
+                : kelas == "8"
+                ? "Delapan (8)"
+                : "Sembilan (9)"}
+            </p>
+            <p>Waktu : {waktu}</p>
+          </AlertDialogDescription>
+          <div className="items-top flex space-x-2 pt-4">
+            <Checkbox
+              id="terms1"
+              checked={!disabled}
+              onCheckedChange={() => isDisabled(!disabled)}
+            />
+            <div className="grid gap-1.5 leading-none">
+              <label
+                htmlFor="terms1"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Mengerjakan soal ujian dengan JUJUR
+              </label>
+              <p className="text-sm text-muted-foreground">
+                Saya berjanji akan mengerjakan soal ujian ini dengan jujur.
+              </p>
+            </div>
+          </div>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Batal</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={disabled}
+            className="text-white bg-neutral-950 dark:text-black dark:bg-white"
+          >
+            <Link href={`${kelas}/${link}`}>Mulai</Link>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
